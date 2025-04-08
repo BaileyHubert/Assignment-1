49bf06dc214c1d842cfa75ba491231f1daf6d0a0
BaileyHubert <bailey.hubert@student.cgcc.edu>
2025-04-08 11:24:42 -0700

    Add files via upload

---
+++ Question3.java
@@ -0,0 +1,17 @@
+
+public class Question3{
+	/*
+	 * Prints "Hello World! when run using a local variable "hi".
+	 * 
+	 * PSUEDOCODE:
+	 * Start program
+	 * Assign local variable "hi" to text "Hello World!"
+	 * Print variable "hi"
+	 * End program
+	 */
+		public static void main(String[] args) {
+	String hi = ("Hello World!");
+	System.out.println(hi);
+		}
+
+	}
---
+++ Question4.java
@@ -0,0 +1,17 @@
+
+public class Question4{
+/*
+ * Prints "Hello World! when run using a static variable "hi".
+ * 
+ * PSUEDOCODE:
+ * Start program
+ * Assign static variable "hi" to text "Hello World!"
+ * Print variable "hi"
+ * End program
+ */
+static String hi = ("Hello World!");
+	public static void main(String[] args) {
+System.out.println(hi);
+	}
+
+}
