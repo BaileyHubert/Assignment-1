7dae2937365f684aebe9228389fcea9cd7936d52
BaileyHubert <bailey.hubert@student.cgcc.edu>
2025-04-08 13:18:50 -0700

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,12 +1,32 @@
+package question5;
+/*
+ * Prints "Hello World!" when run using an instance variable called by an accessor and printed from an object
+ * 
+ * PSUEDOCODE:
+ * Start program
+ * Creates string variable "helloWorld"
+ * Creates new object "MyString"
+ * Calls variable "helloWorld" as "Hello World!"
+ * Object "MyString" prints variable "helloWorld"
+ * "Hello World!" is printed to the console
+ * Program ends.
+ */
 
-public class Question5 {
-	/*
-	 * Print "Hello World!" using an instance variable
-	 */
+public class Main {
+
+String helloWorld;
 
 	public static void main(String[] args) {
-		
+		Main myObject = new Main();
+		myObject.MyString();
 
+	}
+Main (){
+	helloWorld="Hello World!";
+}
+	public void MyString() {
+	
+		System.out.print(helloWorld);
 	}
 
 }
