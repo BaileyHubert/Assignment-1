d2cf63f20d3fcfd9009747d5546d556d85d8bd56
BaileyHubert <bailey.hubert@student.cgcc.edu>
2025-04-08 11:22:41 -0700

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,11 @@
+public class Question2 {
+/**
+ * Prints "Hello World!" when run
+ * Start program
+ * Print "Hello World!"
+ * End Program
+ */
+public static void main(String[] args) {
+	System.out.println("Hello World!");
+}
+}
